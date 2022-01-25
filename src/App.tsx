b9ab59ae741272c9b0bde3d50724231647578b59
DiogoCesar_victsoft <diogo@victsoft.com.br>
2022-01-25 16:52:47 -0300

    add(routers)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+import Routes from './routes';
+
 import './App.css';
 
 class App extends Component {  
   render() {
     return(
         <>
-          
+          <div className="content">
+            <div className="main">
+                <Routes />
+            </div>
+          </div>
         </>
     )
   }
