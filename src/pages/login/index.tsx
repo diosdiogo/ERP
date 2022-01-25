0e1feff7d680fd1d7c6a37edd3484b3b312f6f71
DiogoCesar_victsoft <diogo@victsoft.com.br>
2022-01-25 17:09:33 -0300

    add(page login)

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+
+import { login } from "../../services/auth";
+
+class Login extends Component {
+    render() {
+        return (
+            <Fragment>
+            </Fragment>
+        )
+    }
+}
+export default Login;
